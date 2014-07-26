7597a13e5b65b4b862ff68c132e76c203ce2c283
venkatesh1993 <vgundavarapu@gmail.com>
2014-07-26 19:47:20 +0530

    host

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 $db_host="localhost";
-$db_dbase="bharti";
+$db_dbase="bharti18";
 $db_user="root";
 $db_pass="";
 $con= mysql_connect($db_host,$db_user,$db_pass,$db_dbase) or die("unable to connect to mysql:".mysql_error());
