1de779a997bf7af875ea034d6c1f0fa26ed3a628
venkatesh1993 <vgundavarapu@gmail.com>
2014-07-26 21:55:08 +0530

    thalakay

--- login_php.php
+++ login_php.php
@@ -6,5 +6,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 $name = $_REQUEST['username'];
 $job = $_REQUEST['password'];
+
+
+
 mysql_close($con);
 ?>
