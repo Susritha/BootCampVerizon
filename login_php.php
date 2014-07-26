c8980057a2aa94c98351208f06da474999752fff
venkatesh1993 <vgundavarapu@gmail.com>
2014-07-26 19:55:17 +0530

    chacchipo

--- login_php.php
+++ login_php.php
@@ -1,17 +1,11 @@
+include ("dbconnect.php");
+<script>alert("hi");</script>
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 
-#include ("dbconnect.php");
+
 $name = $_REQUEST['username'];
 $job = $_REQUEST['password'];
-
-<script>alert("hi");</script>
-
-if (!mysql_query($sql)) {
-  die('Error: ' . mysql_error($con));
-}
-echo "1 record added";
-
 mysql_close($con);
 ?>
