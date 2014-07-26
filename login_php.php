cc538c9cc5dabd1141fd9fd2bf9da32ed7b3d84c
venkatesh1993 <vgundavarapu@gmail.com>
2014-07-26 19:57:15 +0530

    ayyo

--- login_php.php
+++ login_php.php
@@ -1,10 +1,9 @@
-include ("dbconnect.php");
+
 <script>alert("hi");</script>
 <?php
+include ("dbconnect.php");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
-
-
 $name = $_REQUEST['username'];
 $job = $_REQUEST['password'];
 mysql_close($con);
